8c8c396e30b27301e476038cea23967f25e8e6d8
unknown <ghaliarose89@gmail.com>
2021-09-05 08:42:28 -0500

    adding input and selestor to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 
 
-var buttonEl= document.querySelector("#save-task");
+var formEl= document.querySelector("#task-form");
 
 var taskToDoEL= document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
-    var listItemEl=document.createElement("li");
-    listItemEl.textContent="This is a new task.";
-    listItemEl.className="task-item";
-    taskToDoEL.appendChild(listItemEl); 
-}
+var createTaskHandler = function(event){
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput=document.querySelector("select[name='task-type']").value; 
 
-buttonEl.addEventListener("click",createTaskHandler);
+    // create list item
+    var listItemEl=document.createElement("li");
+    listItemEl.textContent=taskNameInput;
+    listItemEl.className="task-item";
+
+     // create div to hold task info and add to list item
+     var taskInfoEl=document.createElement("div");
+     taskInfoEl.className= "task-info";
+
+     // add HTML content to div
+    taskInfoEl.innerHTML="<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    taskToDoEL.appendChild(listItemEl);
+   
+    
+};
+
+formEl.addEventListener("submit",createTaskHandler);
