0400b6b7cae29cd26d25ae010c198e20af037405
unknown <ghaliarose89@gmail.com>
2021-09-09 12:05:05 -0500

    loading data frm localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,6 +224,23 @@ var saveTasks = function() {
   localStorage.setItem ("tasks", JSON.stringify(tasks));
 }
 
+var loadTask = function(){
+
+  var savedTasks = localStorage.getItem("tasks");
+
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks);
+
+  // loop through savedTasks array
+  for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+   createTaskEl(savedTasks[i]);
+}
+};
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -232,3 +249,5 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+saveTasks();
